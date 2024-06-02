ce2b9cd6c6ebc227d67daeb0e81228242422888b
yangkro <907661446@qq.com>
2024-06-02 22:22:13 +0800

    add convex

--- app/test/_components/button/page.tsx
+++
@@ -1,7 +0,0 @@
-const Page = ()=>{
-    return (
-        <div>_components</div>
-    )
-}
-
-export default Page
--- app/test/subroute/page.tsx
+++
@@ -1,9 +0,0 @@
-const Page = ()=>{
-    return (
-        <div>
-            sub route Page
-        </div>
-    )
-}
-
-export default Page
