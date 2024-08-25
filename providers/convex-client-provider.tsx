65f7722f730ad1f54ab7e4fc3e983f84cfa5c37b
yangkro <907661446@qq.com>
2024-08-25 15:49:54 +0800

    添加clerk登录

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -2,7 +2,9 @@
 
 import { ClerkProvider,  useAuth } from "@clerk/nextjs"
 import { ConvexProviderWithClerk } from "convex/react-clerk"
-import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react"
+import { AuthLoading, Authenticated, ConvexReactClient, Unauthenticated  } from "convex/react"
+import { Loading } from "@/components/auth/loading"
+import SignInPage from "@/components/auth/sigin"
 import React from "react"
 
 interface ConvexClientProviderProps {
@@ -15,9 +17,17 @@ const convex = new ConvexReactClient(convexUrl)
 
 export const ConvexClientProvider = ({children}:ConvexClientProviderProps): React.ReactNode =>{
     return (
-        <ClerkProvider>
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
             <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-                {children}
+                <Authenticated>
+                    {children}
+                </Authenticated>
+                <AuthLoading>
+                    <Loading/>
+                </AuthLoading>
+                <Unauthenticated>
+                    <SignInPage/>
+                </Unauthenticated>
             </ConvexProviderWithClerk>
         </ClerkProvider>
     )
